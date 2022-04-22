214b399fb41c90814a573e1ec466586dfaf044e6
Luana <Luana@DESKTOP-ODA6GJK>
2022-04-21 22:14:07 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaDoWhile_CSharp-11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EstruturaDoWhile_CSharp_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 10;
+
+            do
+            {
+
+                Console.WriteLine("Maria");
+
+                cont++;
+            }
+            while (cont < 5);
+        }
+    }
+}
